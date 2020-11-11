5a75afb84542a075ab2028936d8f36c9c4a91f3b
Andrew Ha <ah10@live.com.au>
2020-11-12 04:28:09 +0800

    Draggable list of destinations

--- main.js
+++ main.js
@@ -1,88 +1,20 @@
-
     // Initialise some variables
     var directionsService = new google.maps.DirectionsService();
     var num, map, data, j=0;
-    var requestArray = [], renderArray = [];
-
-    // A JSON Array containing some people/routes and the destinations/stops
-    var jsonArray = {
-        "Person 1": ["Perth", "Joondalup", "Gingin"],
-        //"Person 2": ["Exmouth", "Sidmouth", "Taunton", "Crediton", "Okehampton"],
-        //"Person 3": ["Penzance", "Truro", "Bodmin", "Falmouth"]
-    }
+    var requestArray = [], renderArray = [], uiArray = [];
 
     // 16 Standard Colours for navigation polylines
     var colourArray = ['navy', 'grey', 'fuchsia', 'black', 'white', 'lime', 'maroon', 'purple', 'aqua', 'red', 'green', 'silver', 'olive', 'blue', 'yellow', 'teal'];
 
-    // Let's make an array of requests which will become individual polylines on the map.
-    function generateRequests(){
-
-        requestArray = [];
-
-        for (var route in jsonArray){
-            // This now deals with one of the people / routes
-
-            // Somewhere to store the wayoints
-            var waypts = [];
-
-            // 'start' and 'finish' will be the routes origin and destination
-            var start, finish
-
-            // lastpoint is used to ensure that duplicate waypoints are stripped
-            var lastpoint
-
-            data = jsonArray[route]
-
-            limit = data.length
-            for (var waypoint = 0; waypoint < limit; waypoint++) {
-                if (data[waypoint] === lastpoint){
-                    // Duplicate of of the last waypoint - don't bother
-                    continue;
-                }
-
-                // Prepare the lastpoint for the next loop
-                lastpoint = data[waypoint]
-
-                // Add this to waypoint to the array for making the request
-                waypts.push({
-                    location: data[waypoint],
-                    stopover: true
-                });
-            }
-
-            // Grab the first waypoint for the 'start' location
-            start = (waypts.shift()).location;
-            // Grab the last waypoint for use as a 'finish' location
-            finish = waypts.pop();
-            if(finish === undefined){
-                // Unless there was no finish location for some reason?
-                finish = start;
-            } else {
-                finish = finish.location;
-            }
-
-            // Let's create the Google Maps request object
-            var request = {
-                origin: start,
-                destination: finish,
-                waypoints: waypts,
-                travelMode: google.maps.TravelMode.DRIVING
-            };
-
-            // and save it in our requestArray
-            requestArray.push({"route": route, "request": request});
-        }
-
-        processRequests();
-    }
-
     function readRequests(){
         
+        var order = sortable.toArray();
+        
         var uiArray = {"Track 1 ": [
-                document.getElementById("pac-input1").value, 
-                document.getElementById("pac-input2").value,
-                document.getElementById("pac-input3").value,
-                document.getElementById("pac-input4").value]
+                document.getElementById(`pac-input${order[0]}`).value, 
+                document.getElementById(`pac-input${order[1]}`).value,
+                document.getElementById(`pac-input${order[2]}`).value,
+                document.getElementById(`pac-input${order[3]}`).value]
                     };
         
         requestArray = [];
@@ -160,6 +92,8 @@
         function directionResults(result, status) {
             if (status == google.maps.DirectionsStatus.OK) {
 
+                var order = sortable.toArray();
+
                 console.log(result)
 
                 var addresses = 0;
@@ -182,9 +116,9 @@
                 if (Math.floor(arrivalTime%3600/60) > 10) extra00 = "";
                 var arrivalClock = extra0 + Math.floor(arrivalTime/60/60)%24 + ":" + extra00 + Math.floor(arrivalTime%3600/60)
 
-                document.getElementById("routes").innerHTML += "<br>" + document.getElementById("pac-input1").value
+                document.getElementById("routes").innerHTML += "<br>" + document.getElementById(`pac-input${order[0]}`).value
                 for(i=2; i<=addresses; i++)
-                    document.getElementById("routes").innerHTML += " &#8594 " + document.getElementById(`pac-input${i}`).value 
+                    document.getElementById("routes").innerHTML += " &#8594 " + document.getElementById(`pac-input${order[i-1]}`).value 
                 document.getElementById("routes").innerHTML += " (" + Math.floor(timeTaken/60/60) + "Hrs " + Math.floor((timeTaken%3600)/60) + "Mins) "; //+ Math.floor(timeTaken%60) + "Secs) ";
                 document.getElementById("routes").innerHTML += " <b><u>[" + document.getElementById("departTime").value + " &#8594 " + arrivalClock + "]</b></u>";
                 document.getElementById("routes").innerHTML += "<button id='routeNumber" + j + "' onclick='removeRoute(this.id)' style='float: right;'>X</button><br>";
@@ -278,9 +212,6 @@
 
         map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-        // Start the request making
-        //generateRequests()
-
         var defaultBounds = new google.maps.LatLngBounds(
         new google.maps.LatLng(-32,110),
         new google.maps.LatLng(-34,130));
@@ -299,6 +230,10 @@
 
         document.getElementById("departTime").defaultValue = "08:00";
 
+        sortable = new Sortable(sortablelist, {
+            animation: 150,
+            ghostClass: 'sortable-ghost'
+        });
     }
 
     // Get the ball rolling and trigger our init() on 'load'
